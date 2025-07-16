317912c0e904b28700e49afe843b8fe2b0fe7ad6
djckin07182 <69359151+djckin07182@users.noreply.github.com>
2025-07-17 04:08:57 +0800

    Update trait_service.py

--- trait_service.py
+++ trait_service.py
@@ -2,7 +2,7 @@
 import json
 from pathlib import Path
 
-TRAIT_INFO_PATH = Path(__file__).parent / "trait_info_3key.json"
+TRAIT_INFO_PATH = Path(__file__).parent / "trait_info.json"
 
 def load_trait_info():
     with open(TRAIT_INFO_PATH, "r", encoding="utf-8") as f:
